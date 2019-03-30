890f10e1f5a19dace279f44e562cea4e4e79f5f6
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-03-30 15:48:36 +0900

    [Update] Reloader

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require "sinatra/base"
+require 'sinatra/reloader'
 
 class MyApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   get "/" do
     'hello, world!'
   end
